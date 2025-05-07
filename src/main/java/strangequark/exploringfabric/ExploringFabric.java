a582d614aa3ff5767f58a5e0eb07e93d4af04bce
Yasin  Bhimani <masteryasin@ymail.com>
2025-05-08 01:11:17 +0530

    feat: Add custom items (#1)

--- src/main/java/strangequark/exploringfabric/ExploringFabric.java
+++ src/main/java/strangequark/exploringfabric/ExploringFabric.java
@@ -1,16 +1,17 @@
 package strangequark.exploringfabric;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import strangequark.exploringfabric.item.ModItems;
 
 public class ExploringFabric implements ModInitializer {
-	public static final String MOD_ID = "exploringfabric";
+    public static final String MOD_ID = "exploringfabric";
 
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-	}
+    @Override
+    public void onInitialize() {
+        ModItems.registerModItems();
+    }
 }
---
+++ src/main/java/strangequark/exploringfabric/item/ModItems.java
@@ -0,0 +1,36 @@
+package strangequark.exploringfabric.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import strangequark.exploringfabric.ExploringFabric;
+
+public class ModItems {
+
+    public static final Item PINK_GARNET = registerItem("pink_garnet",
+            new Item(new Item.Settings()
+                    .registryKey(RegistryKey.of(RegistryKeys.ITEM,
+                            Identifier.of(ExploringFabric.MOD_ID, "pink_garnet")))));
+    
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet",
+            new Item(new Item.Settings()
+                    .registryKey(RegistryKey.of(RegistryKeys.ITEM,
+                            Identifier.of(ExploringFabric.MOD_ID, "raw_pink_garnet")))));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(ExploringFabric.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        ExploringFabric.LOGGER.info("Registering Mod Items for " + ExploringFabric.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+            entries.add(RAW_PINK_GARNET);
+        });
+    }
+}
