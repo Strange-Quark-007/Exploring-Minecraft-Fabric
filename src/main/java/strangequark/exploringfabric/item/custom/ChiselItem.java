458cf0cf686780418356d92ffe5dcee4297580f4
Yasin  Bhimani <masteryasin@ymail.com>
2025-05-10 01:45:49 +0530

    feat: Add custom advanced item (#6)

---
+++ src/main/java/strangequark/exploringfabric/item/custom/ChiselItem.java
@@ -0,0 +1,58 @@
+package strangequark.exploringfabric.item.custom;
+
+import net.fabricmc.fabric.api.item.v1.EnchantingContext;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.world.World;
+import strangequark.exploringfabric.block.ModBlocks;
+
+import java.util.Map;
+
+public class ChiselItem extends Item {
+    private static final Map<Block, Block> CHISEL_MAP = Map.of(
+            Blocks.STONE, Blocks.STONE_BRICKS,
+            Blocks.END_STONE, Blocks.END_STONE_BRICKS,
+            ModBlocks.RAW_PINK_GARNET_BLOCK, ModBlocks.PINK_GARNET_BLOCK
+    );
+
+    public ChiselItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+
+        World world = context.getWorld();
+        Block clickedBlock = world.getBlockState(context.getBlockPos()).getBlock();
+
+        if (CHISEL_MAP.containsKey(clickedBlock)) {
+            if (!world.isClient()) {
+                world.setBlockState(context.getBlockPos(), CHISEL_MAP.get(clickedBlock).getDefaultState());
+
+                context.getStack().damage(1, (ServerWorld) context.getWorld(), (ServerPlayerEntity) context.getPlayer(), item -> {
+                    assert context.getPlayer() != null;
+                    context.getPlayer().sendEquipmentBreakStatus(item, EquipmentSlot.MAINHAND);
+                });
+
+                world.playSound(null, context.getBlockPos(), SoundEvents.BLOCK_GRINDSTONE_USE, SoundCategory.BLOCKS);
+            }
+        }
+        return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public boolean canBeEnchantedWith(ItemStack stack, RegistryEntry<Enchantment> enchantment, EnchantingContext context) {
+        return super.canBeEnchantedWith(stack, enchantment, context);
+    }
+}
