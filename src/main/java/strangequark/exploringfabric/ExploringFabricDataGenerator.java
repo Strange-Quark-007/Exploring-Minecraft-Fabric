71af7716ac64527a38316a5f2c8704ee05e1cd78
Strange-Quark-007 <masteryasin@ymail.com>
2025-05-11 15:49:22 +0530

    feat: Add data generation

--- src/main/java/strangequark/exploringfabric/ExploringFabricDataGenerator.java
+++ src/main/java/strangequark/exploringfabric/ExploringFabricDataGenerator.java
@@ -2,10 +2,17 @@ package strangequark.exploringfabric;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import strangequark.exploringfabric.datagen.*;
 
 public class ExploringFabricDataGenerator implements DataGeneratorEntrypoint {
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
-	}
+        pack.addProvider(ModBlockTagProvider::new);
+        pack.addProvider(ModItemTagProvider::new);
+        pack.addProvider(ModLootTableProvider::new);
+        pack.addProvider(ModModelProvider::new);
+        pack.addProvider(ModRecipeProvider::new);
+    }
 }
