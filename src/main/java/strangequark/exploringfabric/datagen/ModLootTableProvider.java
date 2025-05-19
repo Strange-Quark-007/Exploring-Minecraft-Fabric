4e4c941edd5d787912d0446e89566b7a916a215b
Strange-Quark-007 <masteryasin@ymail.com>
2025-05-19 18:46:23 +0530

    feat: Add custom crops

--- src/main/java/strangequark/exploringfabric/datagen/ModLootTableProvider.java
+++ src/main/java/strangequark/exploringfabric/datagen/ModLootTableProvider.java
@@ -7,13 +7,16 @@ import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.Item;
 import net.minecraft.loot.LootTable;
+import net.minecraft.loot.condition.BlockStatePropertyLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.ApplyBonusLootFunction;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.predicate.StatePredicate;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import strangequark.exploringfabric.block.ModBlocks;
+import strangequark.exploringfabric.block.custom.CauliflowersBlock;
 import strangequark.exploringfabric.item.ModItems;
 
 import java.util.concurrent.CompletableFuture;
@@ -43,6 +46,12 @@ public class ModLootTableProvider extends FabricBlockLootTableProvider {
         addDrop(ModBlocks.PINK_GARNET_BUTTON);
         addDrop(ModBlocks.PINK_GARNET_PRESSURE_PLATE);
         addDrop(ModBlocks.PINK_GARNET_LAMP);
+
+        BlockStatePropertyLootCondition.Builder cauliflowerCondition =
+                BlockStatePropertyLootCondition.builder(ModBlocks.CAULIFLOWERS)
+                        .properties(StatePredicate.Builder.create().exactMatch(CauliflowersBlock.AGE, CauliflowersBlock.MAX_AGE));
+
+        this.addDrop(ModBlocks.CAULIFLOWERS, this.cropDrops(ModBlocks.CAULIFLOWERS, ModItems.CAULIFLOWER, ModItems.CAULIFLOWER, cauliflowerCondition));
     }
 
     public LootTable.Builder multipleOreDrops(Block drop, Item item, float minDrops, float maxDrops) {
--- src/main/java/strangequark/exploringfabric/item/ModItems.java
+++ src/main/java/strangequark/exploringfabric/item/ModItems.java
@@ -7,6 +7,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import strangequark.exploringfabric.ExploringFabric;
 import strangequark.exploringfabric.armor.ModArmorMaterials;
+import strangequark.exploringfabric.block.ModBlocks;
 import strangequark.exploringfabric.food.ModConsumableComponents;
 import strangequark.exploringfabric.food.ModFoodComponents;
 import strangequark.exploringfabric.item.custom.ChiselItem;
@@ -21,7 +22,7 @@ public class ModItems {
     public static final Item PINK_GARNET = createItem("pink_garnet", settings -> new Item(settings.trimMaterial(ModTrimMaterials.PINK_GARNET)));
 
     public static final Item CHISEL = createItem("chisel", settings -> new ChiselItem(settings.maxDamage(32)));
-    public static final Item CAULIFLOWER = createItem("cauliflower", settings -> new Item(settings.food(ModFoodComponents.CAULIFLOWER, ModConsumableComponents.CAULIFLOWER)));
+    public static final Item CAULIFLOWER = createItem("cauliflower", settings -> new BlockItem(ModBlocks.CAULIFLOWERS, settings.food(ModFoodComponents.CAULIFLOWER, ModConsumableComponents.CAULIFLOWER)));
     public static final Item STARLIGHT_ASHES = createItem("starlight_ashes", Item::new);
 
     public static final Item PINK_GARNET_SWORD = createItem("pink_garnet_sword", settings -> new Item(settings.sword(ModToolMaterials.PINK_GARNET, 3.0F, -2.4F)));
@@ -42,6 +43,7 @@ public class ModItems {
     public static final Item QUARK_ARMOR_TRIM_SMITHING_TEMPLATE = createItem("quark_armor_trim_smithing_template", SmithingTemplateItem::of);
 
     public static final Item QUARK_BOW = createItem("quark_bow", settings -> new BowItem(settings.maxDamage(250)));
+
 
     public static <T extends Item> T createItem(String name, ItemFactory<T> itemCreator) {
         Item.Settings settings = new Item.Settings().registryKey(createRegistryKey(name));
