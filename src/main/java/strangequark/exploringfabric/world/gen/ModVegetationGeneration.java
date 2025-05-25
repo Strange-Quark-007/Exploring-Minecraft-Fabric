9948a5e4242e0a6205b97052997965040c419cc7
Yasin  Bhimani <masteryasin@ymail.com>
2025-05-25 11:53:05 +0530

    feat: Bush Generation (#36)

---
+++ src/main/java/strangequark/exploringfabric/world/gen/ModVegetationGeneration.java
@@ -0,0 +1,13 @@
+package strangequark.exploringfabric.world.gen;
+
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.gen.GenerationStep;
+import strangequark.exploringfabric.world.ModVegetationPlacedFeatures;
+
+public class ModVegetationGeneration {
+    public static void generateVegetation() {
+        BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.PLAINS, BiomeKeys.FOREST), GenerationStep.Feature.VEGETAL_DECORATION, ModVegetationPlacedFeatures.HONEY_BERRY_BUSH);
+    }
+}
